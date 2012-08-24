86726b1ce594513d3490d0213599ec20ea470126
jimmytidey <jimmytidey@gmail.com>
2012-08-24 16:23:44 +0100

    automated commit

---
+++ html/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+	<meta charset=utf-8 />
+
+
+	<title>LANDING V1</title>
+	<meta name="google-site-verification" content="zQd8pef_mazi8KBab_uSbTF8BsxNJAE62AUWO2hgUho" />
+	<meta name="description" content="Jimmy Tidey - Product Manager and Creative Technologist" />
+	
+
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+	
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+    
+<head>
+    
+<body id='v1'>
+    <div id='container'>        
+        <h1>Here are several versions of the Wines &amp; Spirits home page</h1>
+        
+        <p><a href='v1.php' target='_blank' >Logo with URL</a><p>
+        <p><a href='v2.php' target='_blank' >Logo without URL</a><p>
+        
+        <div id="surveyMonkeyInfo"><div><script src="http://www.surveymonkey.com/jsEmbed.aspx?sm=S_2fRbwat1sWrUrsy3zybmcQ_3d_3d"> </script></div></div>        
+
+        <p><a href='v1.php' target='_blank' >Without tabs</a><p>
+        <p><a href='v1.php' target='_blank' >With tabs</a><p>
+        <div id="surveyMonkeyInfo"><div><script src="http://www.surveymonkey.com/jsEmbed.aspx?sm=S_2fRbwat1sWrUrsy3zybmcQ_3d_3d"> </script></div></div>        
+
+        <p><a href='v1.php' target='_blank' >Version 1 header text</a><p>
+        <p><a href='v1.php' target='_blank' >Version 2 header text</a><p>
+        <div id="surveyMonkeyInfo"><div><script src="http://www.surveymonkey.com/jsEmbed.aspx?sm=S_2fRbwat1sWrUrsy3zybmcQ_3d_3d"> </script></div></div>        
+
+        <p><a href='v1.php' target='_blank' >With side bar</a><p>
+        <p><a href='v1.php' target='_blank' >Without side bar</a><p>
+        <div id="surveyMonkeyInfo"><div><script src="http://www.surveymonkey.com/jsEmbed.aspx?sm=S_2fRbwat1sWrUrsy3zybmcQ_3d_3d"> </script></div></div>        
+
+        <p><a href='v1.php' target='_blank' >Version 1 of quick link boxes</a><p>
+        <p><a href='v1.php' target='_blank' >Version 2 of quick link boxes</a><p>
+        <div id="surveyMonkeyInfo"><div><script src="http://www.surveymonkey.com/jsEmbed.aspx?sm=S_2fRbwat1sWrUrsy3zybmcQ_3d_3d"> </script></div></div>        
+
+
+
+	</div>
+</body>
+</html>
