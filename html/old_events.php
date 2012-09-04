18caf2baa2bec7dc6fc4a9a1c0169d661800bcf6
jimmytidey <jimmytidey@gmail.com>
2012-09-04 14:37:04 +0100

    automated commit

---
+++ html/old_events.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+
+	<meta charset=utf-8 />
+
+
+	<title>LANDING PAGE</title>
+	<meta name="google-site-verification" content="zQd8pef_mazi8KBab_uSbTF8BsxNJAE62AUWO2hgUho" />
+	<meta name="description" content="Jimmy Tidey - Product Manager and Creative Technologist" />
+	
+
+	<link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+	
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
+<head>
+    
+<body id='old_events'>
+    <div id='container'>        
+        <img src='resources/header.png' />
+        
+        <a id='directory' class='tab' />Directory</a>        
+        <a id='offer' class='tab'   >Offers &amp; Tenders</a>
+        <a id='comp' class='tab' />Events</a>
+        <a id='press' class='tab' />Press &amp; News</a>
+        
+            
+        
+        <div id='content_holder'>
+            <div id='home_text' >
+
+            </div>
+            
+        </div>
+        
+        
+        
+        <div id='personal_info'>
+            <iframe src="https://www.google.com/calendar/embed?src=gc1bopmh3c5n0ogvlo6ceujlkc%40group.calendar.google.com&ctz=Europe/London" style="border: 0" width="680" height="600" frameborder="0" scrolling="no"></iframe>             
+            
+        </div>        
+                
+        <div id='sidebar'>
+
+            <p>Add Event or Competition</p>
+
+        </div>
+        
+
+            <img src='resources/footer.png' id='footer'>
+	</div>
+</body>
+</html>
